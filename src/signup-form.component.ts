1b21794bc5c85bcb3eccb79e3b5005bbb709edce
padmagkurdgi <padmagkurdgi@mail.com>
2017-05-24 16:28:05 -0500

    disabled submit button when the form is invalid

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,15 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    <form (submit) = "OnSubmit(emailField)" novalidate>
+    <form (submit) = "OnSubmit(emailField)" novalidate #form="ngForm">
     <div class = "form-group">
       <label> Email </label><br/>
       <input type = "text" class = "form-control" #emailField ="ngModel"
       [(ngModel)]= "email" name = "email" required pattern=".+@.+" > <br/><br/>
       <p *ngIf= "emailField.touched && emailField.invalid && emailField.errors.required" class = "alert alert-danger"> Email is required </p>
       <p *ngIf= "emailField.touched && emailField.invalid && emailField.errors.pattern" class = "alert alert-danger"> Email pattern is invalid </p>
-     
-      <button type="submit" class = "btn btn-primary"> Sign Up </button>
+       <label> Password </label><br/>
+      <input type = "password" class = "form-control" #passwordField ="ngModel"
+      [(ngModel)]= "password" name = "password" required  > <br/><br/>
+      <button type="submit" class = "btn btn-primary" [disabled]="form.invalid"> Sign Up </button>
      </div> 
   </form>
   `,
@@ -23,8 +25,9 @@ import { Component } from '@angular/core';
 })
 export class SignupFormComponent {
   email='';
-  OnSubmit(emailfield){
-    console.log("emailField: " + emailfield);
+  OnSubmit(form){
+    console.log("emailField: " + form);
+    form.resetForm();
 
   }
 }
