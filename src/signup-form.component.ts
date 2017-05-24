056c0f05cbe99b152e9c5b6593f0826d381ada7f
padmagkurdgi <padmagkurdgi@mail.com>
2017-05-24 07:51:16 -0500

    added css styles

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,15 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    <form>
+    <form (submit) = "OnSubmit()">
     <div class = "form-group">
       <label> Email </label><br/>
-      <input type = "text" class = "form-control" > <br/><br/>
-      <button type="submit" class = "btn btn-primary"> Submit </button>
+      <input type = "text" class = "form-control" [(ngModel)]= "email" name = "email" required pattern=".+@.+" > <br/><br/>
+      <button type="submit" class = "btn btn-primary"> Sign Up </button>
      </div> 
   </form>
-  `
+  `,
+  styles:[`
+    input.ng-invalid{
+        border: solid red 2px;
+    }
+  `]
 })
 export class SignupFormComponent {
+  email='';
+  OnSubmit(){
+    console.log("Should submit  " + this.email);
 
+  }
 }
