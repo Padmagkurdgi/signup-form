8b70d9d2cc99fe005ce46cecda32a47aadd0942b
padmagkurdgi <padmagkurdgi@mail.com>
2017-05-23 21:01:37 -0500

    signup-from creation

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    To do...
+    <form>
+    <div class = "form-group">
+      <label> Email </label><br/>
+      <input type = "text" class = "form-control" > <br/><br/>
+      <button type="submit" class = "btn btn-primary"> Submit </button>
+     </div> 
+  </form>
   `
 })
 export class SignupFormComponent {
