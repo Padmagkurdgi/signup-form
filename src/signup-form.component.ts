fccadaecebd2ab371fcb29e1532213e3d5b91917
padmagkurdgi <padmagkurdgi@mail.com>
2017-05-24 15:44:27 -0500

    Added vaildation messages

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,24 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    <form (submit) = "OnSubmit()">
+    <form (submit) = "OnSubmit(emailField)" novalidate>
     <div class = "form-group">
       <label> Email </label><br/>
-      <input type = "text" class = "form-control" [(ngModel)]= "email" name = "email" required pattern=".+@.+" > <br/><br/>
+      <input type = "text" class = "form-control" #emailField ="ngModel"
+      [(ngModel)]= "email" name = "email" required pattern=".+@.+" > <br/><br/>
+      <p *ngIf= "emailField.touched && emailField.invalid && emailField.errors.required" class = "alert alert-danger"> Email is required </p>
+      <p *ngIf= "emailField.touched && emailField.invalid && emailField.errors.pattern" class = "alert alert-danger"> Email pattern is invalid </p>
+     
       <button type="submit" class = "btn btn-primary"> Sign Up </button>
      </div> 
   </form>
   `,
   styles:[`
-    input.ng-invalid{
+    input.ng-touched.ng-invalid{
         border: solid red 2px;
     }
   `]
 })
 export class SignupFormComponent {
   email='';
-  OnSubmit(){
-    console.log("Should submit  " + this.email);
+  OnSubmit(emailfield){
+    console.log("emailField: " + emailfield);
 
   }
 }
